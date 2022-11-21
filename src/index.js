0c880e26f985384737ff33fa82502fc90a9dfbe0
Natasha <natkamonte1992@gmail.com>
2022-11-21 13:36:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,32 +8,73 @@ const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
 const div = document.querySelector('.country-info');
 
-input.addEventListener('input', debounce(onSearch, 300));
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(event) {
-    event.preventDefault();
-    const searchingCountry = input.value.trim();
+  event.preventDefault();
+  const searchingCountry = input.value.trim();
 
-    if (!searchingCountry) {
-        Notiflix.Notify.info('Please, enter country name');
-        return
+  if (!searchingCountry) {
+    Notiflix.Notify.info('Please, enter country name');
+    return;
+  }
+
+  fetchCountries(searchingCountry).then(data => {
+    console.log(data);
+
+    // if (searchingCountry >= 10) {
+    //   Notiflix.Notify.info(
+    //     'Too many matches found. Please enter a more specific name.'
+    //   );
+    // }
+    // if (searchingCountry > 2 && searchingCountry < 10) {
+    //   listMarkUp(data);
+    // }
+    if (searchingCountry === 1) {
+     return  cardMarkUp(data)
     };
-    fetchCountries(searchingCountry).then(data => markUp(data));
+    return;
+  });
 }
 
-function markUp(array) {
-    const newMarkUp = array
-      .map(
-        item => `<li>
+function cardMarkUp(array) {
+  const markUp = array
+    .map(
+      item => `<div class="country_card">
         <img class="flag" src="${item.flags.svg}" alt="" width="60" height="30">
         <h2 class="off_name">${item.altSpellings}</h2>
         <p>Capital : ${item.capital}</p>
         <p>Population : ${item.population}</p>
-        <p>Languages : ${Object.values(item.languages)}</p>
-      </li>`
-      )
-      .join('');
-    
-    list.innerHTML = newMarkUp;
-    console.log(newMarkUp)
+        <p>Languages : ${Object.values(item.languages)}</p></div>`
+    )
+    .join('');
+
+  div.innerHTML = markUp;
+  console.log(markUp);
+}
+cardMarkUp([
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/ua.png',
+      svg: 'https://flagcdn.com/ua.svg',
+    },
+    capital: ['Kyiv'],
+    altSpellings: ['UA', 'Ukrayina'],
+    languages: {
+      ukr: 'Ukrainian',
+    },
+    population: 44134693,
+  },
+]);
+
+function listMarkUp(array) {
+  const newMarkUp = array
+    .map(
+      item => `<li>
+        <img class="flag" src="${item.flags.svg}" alt="" width="60" height="30">
+        <h2 class="off_name">${item.altSpellings}</h2></li>`
+    )
+    .join('');
+
+  list.innerHTML = newMarkUp;
 }
