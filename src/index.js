743f605ef90d94de5d481c4e5c5ebd47772e1830
Natasha <natkamonte1992@gmail.com>
2022-11-21 18:59:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
-var debounce = require('lodash.debounce');
 const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
 const div = document.querySelector('.country-info');
@@ -11,29 +11,29 @@ const div = document.querySelector('.country-info');
 input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(event) {
-  event.preventDefault();
-  const searchingCountry = input.value.trim();
+  const searchingCountry = event.target.value.trim();
 
-  if (!searchingCountry) {
+  if (searchingCountry === '') {
+    div.innerHTML = '';
+    list.innerHTML = '';
     Notiflix.Notify.info('Please, enter country name');
     return;
   }
 
   fetchCountries(searchingCountry).then(data => {
-    div.innerHTML = '';
-    list.innerHTML = '';
-    
-    if (searchingCountry.length > 10) {
-     return Notiflix.Notify.info(
+    if (data.length > 10) {
+      return Notiflix.Notify.info(
         'Too many matches found. Please enter a more specific name.'
       );
-    } else if (searchingCountry.length > 2 && searchingCountry.length < 10) {
+    } else if (data.length === 1) {
+      list.innerHTML = '';
       div.innerHTML = cardMarkUp(data);
       // list.innerHTML = listMarkUp(data);
-    } else  {
+    } else {
+      div.innerHTML = '';
       list.innerHTML = listMarkUp(data);
       // div.innerHTML = cardMarkUp(data);
-    };
+    }
   });
 }
 
@@ -46,13 +46,13 @@ function cardMarkUp(array) {
         <p>Capital : ${item.capital}</p>
         <p>Population : ${item.population}</p>
         <p>Languages : ${Object.values(item.languages)}</p></div>`
-      )
-    .join('')
-};
-
+    )
+    .join('');
+}
 
 function listMarkUp(array) {
-  return array.map(
+  return array
+    .map(
       item => `<li>
         <img class="flag" src="${item.flags.svg}" alt="" width="60" height="30">
         <h2 class="off_name">${item.name.official}</h2></li>`
